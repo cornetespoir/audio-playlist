4a21accffb100586d8820f0df23a6db9f653387b
Bridgette Rue <bridgetterue@egg.design>
2025-05-15 21:17:16 -0500

    actually making the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-"use client"
-import { SearchDataProvider } from "@/context/SearchDataContext";
-import { Search } from "@/components/Search/Search";
+'use client'
+import { SearchDataProvider } from '@/context/SearchDataContext';
+import { Search } from '@/components/Search/Search';
+import { Start } from '@/components/Search/Start';
 
 export default function Home() {
   return (
-   <>
-    <SearchDataProvider>
-      <Search />
-    </SearchDataProvider>
-    <header className='flex-wrap full-width'>
-      <h1>Make a playlist out of any blog</h1>
-    </header>
-   </>
+    <>
+      <SearchDataProvider>
+        <div className='searchbar flex-wrap justify-cetner align-center full-width'>
+          <Search />
+        </div>
+        <header className='heading flex-wrap jusitfy-center align-center full-width'>
+            <h1>Make a playlist out of any blog</h1>
+            <p>Search for a valid tumblr username to get all of their audio posts.</p>
+            <Start />
+          </header>
+      </SearchDataProvider>
+    </>
   )
 }
