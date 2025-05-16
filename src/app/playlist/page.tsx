9e4fcb4ea732c2b4b08e722f7a13c4b5f7cdcd37
Bridgette Rue <bridgetterue@egg.design>
2025-05-15 21:17:59 -0500

    page templates

---
+++ src/app/playlist/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { SearchDataProvider } from '@/context/SearchDataContext';
+import { Search } from '@/components/Search/Search';
+import { PlaylistWrapper } from '@/components/Search/PlaylistWrapper';
+import { NowPlaying } from '@/components/Player/NowPlaying';
+import { Player } from '@/components/Player/Player';
+import { Blog } from '@/components/Player/Blog';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <SearchDataProvider>
+      <div className='searchbar flex-wrap justify-center align-center full-width'>
+        <nav className='flex-wrap justify-center align-center'>
+          <Link href='/'>Home</Link>
+        </nav>
+        <Search />
+      </div>
+      <Blog />
+      <header className='flex-wrap full-width'>
+        <NowPlaying />
+      </header>
+      <main className='full-width'>
+        <PlaylistWrapper />
+      </main>
+      <Player />
+    </SearchDataProvider>
+  )
+}
