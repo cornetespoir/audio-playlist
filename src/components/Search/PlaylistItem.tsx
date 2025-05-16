66d1d18162ed6fa9f4497d3c9f5666a72fa0c045
Bridgette Rue <bridgetterue@egg.design>
2025-05-15 21:18:54 -0500

    the playlist

---
+++ src/components/Search/PlaylistItem.tsx
@@ -0,0 +1,52 @@
+import { useSearchDataContext } from '@/context/SearchDataContext'
+import { PostData } from '@/types/types'
+import clsx from 'clsx'
+import { Post } from '../Posts/Post'
+import { useRef, MouseEvent, useState } from 'react'
+
+interface PlaylistItemProps {
+    data: PostData
+    index: number
+}
+
+interface Coordinates {
+    x: number
+    y: number
+}
+function PlaylistItem({data, index}: PlaylistItemProps) {
+    const {setIndex, index: activeIndex} = useSearchDataContext()
+    const playlistItemRef = useRef<HTMLDivElement | null>(null)
+    const clickRef = useRef<HTMLDivElement | null>(null)
+
+    const [coordinates, setCoordinates] = useState<Coordinates>({x: 0, y: 0})
+
+    const handleSetActive = (e: MouseEvent<HTMLDivElement>) :void => {
+        setIndex(index)
+        if (playlistItemRef.current != null) {
+            clickRef.current?.classList.remove('pulse')
+            const x = e.pageX - playlistItemRef.current.offsetLeft
+            const y = e.pageY - playlistItemRef.current.offsetTop
+            setCoordinates({x, y})
+            clickRef.current?.classList.add('pulse')
+        }
+    }
+    const active = activeIndex === index
+    const activeClasses = clsx(
+        'playlist-item flex-wrap align-center rounded',
+        active && 'active'
+    )
+    return (
+        <div
+            ref={playlistItemRef}
+            onClick={handleSetActive}
+            className={activeClasses}
+        >
+            <div ref={clickRef} className='click' style={{top: coordinates.y, left: coordinates.x}}></div>
+            <Post data={data} isPlaylist />
+        </div>
+    )
+}
+
+export {
+    PlaylistItem
+}
---
+++ src/components/Search/PlaylistWrapper.tsx
@@ -0,0 +1,48 @@
+import { useSearchDataContext } from '@/context/SearchDataContext'
+import { ReactElement } from 'react'
+import { isArrayEmpty, isStringEmpty } from '@/helpers/isEmpty'
+import { PlaylistItem } from './PlaylistItem'
+
+function PlaylistWrapper(): ReactElement | null {
+    const { loading, error, postData, tag, setTag, setOffset, setIndex, endOfResults } = useSearchDataContext()
+   
+    if (error) {
+        return null
+    }
+
+    if (!loading && !isStringEmpty(tag) && isArrayEmpty(postData ?? [])) {
+        return (
+            <div className='not-found flex-wrap align-center justify-center'>No audio posts detected</div>
+        )
+    }
+
+    function startOver() {
+        setIndex(0)
+        setTag(tag)
+        setOffset(0)
+    }
+
+    return (
+        <div className='playlist-wrapper'>
+            {loading && (
+              <div className='full-width flex-wrap justify-center'>
+                  <div className='lds-ellipsis'><div></div><div></div><div></div><div></div></div>
+             </div>
+            )}
+            {postData?.map((data, index) => (
+                <PlaylistItem key={data.id} data={data} index={index} />
+            ))}
+            {endOfResults && !error && !loading && (
+                <div className='end-of-results flex-wrap justify-center full-width'>
+                    <h3>You&apos;ve reached the end of the playlist!</h3>
+                    <p>Start over from the beginning?</p>
+                    <button onClick={startOver}>Restart playlist</button>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export {
+    PlaylistWrapper
+}
